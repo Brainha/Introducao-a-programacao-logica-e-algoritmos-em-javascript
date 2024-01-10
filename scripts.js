61752001208babf5ce9f0926694085c271b89712
Braian <braiannickolas12@gmail.com>
2024-01-10 08:42:09 -0300

    Jogo da adivinhacao
    Aula165

---
+++ scripts.js
@@ -0,0 +1,26 @@
+/*
+  ** Jogo da advinhacao
+
+    Apresente a mensagem ao usuario:
+    "Advinhe o numero que estou pensando? esta entre 0 e 10"
+
+    Crie uma logica para gerar um numero aleatorio e verificar se o numero digitado e o mesmo que o aleatorio gerado pelo sistema
+
+    Enquanto o usuario nao adivinhar o numero, mostra a mensagem:"ERRo, tente novamente:"
+
+    Caso o usuario acerte o numero, apresentar a mensagem: "Parabens! Voce adivinhou o numero em x tentativas"
+
+    Substitua o "x" da m1ensagem, pelo numero de tentativas
+  */
+
+let result = prompt("Adivinhe o numero que estou pensando? Esta entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
+
+let xAttempts = 0;
+
+while (Number(result) != randomNumber) {
+  result = prompt("Erro, tente novamente:")
+  xAttempts++
+}
+
+alert(`Parabens! O numero q eu pensei foi ${randomNumber} e voce adivinhou o numero em ${xAttempts} tentativas`)
